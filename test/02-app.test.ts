baa4850e5d0b64869a4ec38f591ee0318f2561c1
Pier Fumagalli <pier@usrz.com>
2025-02-14 18:09:34 +0100

    Test new datetime and number formats, also including `$t`, `$d`, ...

--- test/02-app.test.ts
+++ test/02-app.test.ts
@@ -1,9 +1,10 @@
 import { describe } from 'node:test'
 
 import { expect, it } from 'vitest'
-import { createApp } from 'vue'
+import { createApp, nextTick } from 'vue'
 
 import { i18n, useTranslator } from '../lib'
+import TestComponent from './test.vue'
 
 describe('I18n Plugin with Vue Apps', () => {
   const date = new Date(1234567890123)
@@ -11,11 +12,13 @@ describe('I18n Plugin with Vue Apps', () => {
   it('should initialize the plugin with options', () => {
     const app = createApp({}).use(i18n, {
       defaultLanguage: 'en',
-      formats: {
-        dateOnlyFormat: { year: 'numeric', month: '2-digit', day: '2-digit', timeZone: 'UTC' },
-        timeOnlyFormat: { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
-        dateTimeFormat: { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
-        numberFormat: { style: 'currency', currency: 'USD' }, // bogus, all numbers are currencies, but it's just a test
+      dateTimeFormats: {
+        default: { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
+        date: { year: 'numeric', month: '2-digit', day: '2-digit', timeZone: 'UTC' },
+        time: { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
+      },
+      numberFormats: {
+        default: { style: 'currency', currency: 'USD' },
       },
       translations: {
         hello: {
@@ -41,8 +44,8 @@ describe('I18n Plugin with Vue Apps', () => {
       expect(translator.tc('cats', 2)).toBe('$2.00 cats, $2.00, really $2.00')
 
       expect(translator.d(date)).toBe('02/13/2009, 23:31:30')
-      expect(translator.d.date(date)).toBe('02/13/2009')
-      expect(translator.d.time(date)).toBe('23:31:30')
+      expect(translator.d(date, 'date')).toBe('02/13/2009')
+      expect(translator.d(date, 'time')).toBe('23:31:30')
       expect(translator.n(1234.56)).toBe('$1,234.56')
 
       translator.locale = new Intl.Locale('de-DE')
@@ -52,10 +55,50 @@ describe('I18n Plugin with Vue Apps', () => {
       expect(translator.tc('cats', 2)).toBe('2,00\xA0$ Katzen, 2,00\xA0$, wirklich 2,00\xA0$')
 
       expect(translator.d(date)).toBe('13.02.2009, 23:31:30')
-      expect(translator.d.date(date)).toBe('13.02.2009')
-      expect(translator.d.time(date)).toBe('23:31:30')
+      expect(translator.d(date, 'date')).toBe('13.02.2009')
+      expect(translator.d(date, 'time')).toBe('23:31:30')
       expect(translator.n(1234.56)).toBe('1.234,56\xA0$')
     })
+  })
+
+  it('should use the $t, $d, $n, ... aliases in templates', async () => {
+    const app = createApp(TestComponent).use(i18n, {
+      defaultLanguage: 'en',
+      dateTimeFormats: {
+        default: { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
+        date: { year: 'numeric', month: '2-digit', day: '2-digit', timeZone: 'UTC' },
+        time: { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false, timeZone: 'UTC' },
+      },
+      numberFormats: {
+        default: { style: 'currency', currency: 'USD' },
+      },
+      translations: {
+        hello: {
+          'en': 'Hello, World!',
+          'de': 'Hallo, Welt!',
+          'de-DE': 'Hallo, Deutschland!',
+          'de-AT': 'Hallo, Österreich!',
+        },
+        cats: {
+          en: ' no cats | one cat | {n} cats, {n}, really {n} ',
+          de: ' keine Katzen | eine Katze | {n} Katzen, {n}, wirklich {n} ',
+        },
+      },
+    })
+
+    document.body.innerHTML = '<div id="app"></div>'
+    app.mount('#app')
+
+    await nextTick()
+
+    expect(document.querySelector('#t')?.innerHTML).toBe('Hello, World!')
+    expect(document.querySelector('#tc0')?.innerHTML).toBe('no cats')
+    expect(document.querySelector('#tc1')?.innerHTML).toBe('one cat')
+    expect(document.querySelector('#tc2')?.innerHTML).toBe('$2.00 cats, $2.00, really $2.00')
+    expect(document.querySelector('#d')?.innerHTML).toBe('02/13/2009, 23:31:30')
+    expect(document.querySelector('#ddate')?.innerHTML).toBe('02/13/2009')
+    expect(document.querySelector('#dtime')?.innerHTML).toBe('23:31:30')
+    expect(document.querySelector('#n')?.innerHTML).toBe('$1,234.56')
   })
 
   it('should initialize the plugin with a language', () => {
